86899a056773a65c704b13d26da83c19c795f405
Mariia <114136940+Maria-Shymanska@users.noreply.github.com>
2025-01-14 15:03:36 +0100

    add dependencis

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import "modern-normalize/modern-normalize.css";
+// import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Route, Routes } from "react-router-dom";
+// import { AppBar } from "./components/AppBar/AppBar";
+// import { refreshUser } from "./store/auth/operations";
+// import { selectIsRefreshing } from "./store/auth/selectors";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+// const App = () => {
+//   const dispatch = useDispatch();
+//   const isRefreshing = useSelector(selectIsRefreshing);
 
-export default App
+//   useEffect(() => {
+//     dispatch(refreshUser());
+//   }, [dispatch]);
+// };
+
+// export default App;
